61d8af3daa9229653e1133e4bfa06c0ca3830ea9
Raghavan789 <mavkrishnaraghavan@gmail.com>
2024-06-23 18:49:19 +0530

    login page

--- firstapp/routes/web.php
+++ firstapp/routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
     return view('welcome');
-    /*
-    import this for using the below code
-    use App\Http\Controllers\DashboardController;
-    the below code says that when ever you get the request u jsut pass it to the index fucntion in the http controller some file name
-    */
 });
-Route::get('/',[DashboardController::class , 'index' ]);
 
-Route::get('/profile',[ProfileController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
